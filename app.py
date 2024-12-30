b8c0e3205a214e1cf00d5bb73c36ae1c98bca9ba
quang <nguyenthequang080203@gmail.com>
2024-12-30 23:35:17 +0700

    add prod

--- app.py
+++ app.py
@@ -243,4 +243,4 @@ def get_syllabus_list():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=8017)
